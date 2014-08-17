ba04e021ba9b41dd1be2a1cc158c02fa7d12f300
invlpg <invlpg@gmail.com>
2014-08-17 14:20:06 +0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pip-autoremove",
-    version="0.1.0",
+    version="0.2.0",
     description="Remove a package and its unused dependencies",
     long_description=open('README.rst').read(),
     py_modules=["pip_autoremove"],
