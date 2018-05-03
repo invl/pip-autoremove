5b3cf178678c16b78d0d489066e4dd955d21e535
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2018-05-03 11:33:52 -0600

    Allow `pip freeze` style output
    Use `--freeze` option on its own or with `--list`.
    
    Fixes invl#12

--- pip_autoremove.py
+++ pip_autoremove.py
@@ -96,6 +96,10 @@ def show_dist(dist):
     print('%s %s (%s)' % (dist.project_name, dist.version, dist.location))
 
 
+def show_freeze(dist):
+    print(dist.as_requirement())
+
+
 def remove_dist(dist):
     subprocess.check_call(["pip", "uninstall", "-y", dist.project_name])
 
@@ -126,8 +130,8 @@ def requires(dist):
 def main(argv=None):
     parser = create_parser()
     (opts, args) = parser.parse_args(argv)
-    if opts.leaves:
-        list_leaves()
+    if opts.leaves or opts.freeze:
+        list_leaves(opts.freeze)
     elif opts.list:
         list_dead(args)
     else:
@@ -142,10 +146,13 @@ def get_leaves(graph):
     return filter(is_leaf, graph)
 
 
-def list_leaves():
+def list_leaves(freeze=False):
     graph = get_graph()
     for node in get_leaves(graph):
-        show_dist(node)
+        if freeze:
+            show_freeze(node)
+        else:
+            show_dist(node)
 
 
 def create_parser():
@@ -162,6 +169,9 @@ def create_parser():
     parser.add_option(
         '-y', '--yes', action='store_true', default=False,
         help="don't ask for confirmation of uninstall deletions.")
+    parser.add_option(
+        '-f', '--freeze', action='store_true', default=False,
+        help="list leaves (packages which are not used by any others) in requirements.txt format")
     return parser
 
 
