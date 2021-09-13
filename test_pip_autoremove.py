08e73d08b306ea88367977bb6e4fbfc1ea2f26c7
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2021-09-13 02:44:47 -0600

    Slightly better test case

--- test_pip_autoremove.py
+++ test_pip_autoremove.py
@@ -8,16 +8,27 @@ import pip_autoremove
 def test_find_all_dead():
     graph = {
         "Flask": set(),
-        "Jinja2": {"Flask"},
+        "Jinja2": {"Flask", "Sphinx"},
         "MarkupSafe": {"Jinja2"},
         "Werkzeug": {"Flask"},
         "itsdangerous": {"Flask"},
+        "Sphinx": set(),
         "pip": set(),
         "setuptools": set(),
     }
     start = {"Flask"}
-    expected = {"Flask", "Jinja2", "MarkupSafe", "Werkzeug", "itsdangerous"}
+    expected = {"Flask", "Werkzeug", "itsdangerous"}
     dead = pip_autoremove.find_all_dead(graph, start)
+    assert dead == expected
+
+    start = {"Sphinx"}
+    dead = pip_autoremove.find_all_dead(graph, start)
+    assert dead == start
+
+    start = {"Sphinx", "Flask"}
+    expected = {"Flask", "Werkzeug", "itsdangerous", "Sphinx", "Jinja2", "MarkupSafe"}
+    dead = pip_autoremove.find_all_dead(graph, start)
+    print(dead)
     assert dead == expected
 
 
